2a6b08b1f964f8de5c5569006aa68e7502f1e1f5
Christian D. Rodriguez <christiandevcode@gmail.com>
2016-03-26 00:39:56 -0400

    header fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 $(window).load(function() {
 	$("#loader").fadeOut("slow");
 });
+$(document).ready(function(){
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+   // $('a.page-scroll').bind('click', function(event) {
+   //    var $anchor = $(this);
+   //    $('html, body').stop().animate({
+   //       scrollTop: ($($anchor.attr('href')).offset().top - 48)
+   //    }, 1250, 'easeInOutExpo');
+   //    event.preventDefault();
+   // });
+
+	// Highlight the top nav as scrolling occurs
+   $('body').scrollspy({
+      target: '.navbar-fixed-top',
+      offset: 51
+   });
+
+	// Closes the Responsive Menu on Menu Item Click
+   $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-toggle:visible').click();
+   });
+
+   // Offset for Main Navigation
+   $('.navbar-default').affix({
+      offset: {
+         top: 100
+      }
+   });
+});
